be62bac840d51eadfc4e7b78f07e4da5e5e08ed1
imnothur <148865874+imnothur@users.noreply.github.com>
2024-09-24 23:00:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'educação fisica',
+    'quem é considerado o rei do futebol?',
+    'o jogador Pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da venezuela?',
+    'Caracas'
+)
+
+criaCartao(
+    'Historia',
+    'em qual ano começou a guerra fria?',
+    ' em 1947  ' 
+
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em ingles?',
+    'goodbye'
+)
